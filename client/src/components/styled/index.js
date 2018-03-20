c71ffda84e1c3ecdaf2807b930812e9822f604a6
Jake Webb <jakewebbsite@gmail.com>
2018-03-19 22:53:07 -0500

    working styled component Loader

--- client/src/components/styled/index.js
+++ client/src/components/styled/index.js
@@ -1,16 +1,25 @@
 import styled from 'styled-components'
 
 export const Loader = styled.div`
-    display: ${props => props.loading ? 'block' : 'block'};
-    position: absolute;
-    background-image: url('images/spinner.svg');
-    z-index: 999;
-    width: 15%;
-    min-width: 100px;
-    max-width: 180px;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    margin: auto;
+    display: ${props => !props.loading ? 'block' : 'none'};
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 998;
+    background-color: rgb(0, 0, 0, 0.8);
+    &:after {
+        position: absolute;
+        content: url('images/spinner.svg');
+        z-index: 999;
+        width: 15%;
+        min-width: 100px;
+        max-width: 180px;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        margin: auto;
+    }
 `
