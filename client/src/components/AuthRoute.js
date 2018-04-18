8342b0bd99feb4c0d0ec30b180534542126326f4
Jake Webb <jakewebbsite@gmail.com>
2018-04-18 00:00:09 -0500

    stop the prettier!

--- client/src/components/AuthRoute.js
+++ client/src/components/AuthRoute.js
@@ -3,20 +3,13 @@ import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
 
 const AuthRoute = ({ component: Component, isLoggedIn, ...rest }) => {
-	return (
-		<Route
-			{...rest}
-			render={props =>
-				isLoggedIn ? (
-					<Component {...props} />
-				) : (
-					<Redirect
-						to={{ pathname: '/login', state: { from: props.location } }}
-					/>
-				)
-			}
-		/>
+	const authCheck = props => (
+		isLoggedIn
+		? <Component {...props} />
+		: <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
 	)
+
+	return <Route {...rest} render={authCheck} />
 }
 
 export default AuthRoute
